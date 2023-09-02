e5c5c79cb91e8bd2db2d3e99d8f74910b04480c1
Vijaykumar Kumavat <vij2080123@paycor.com>
2023-09-02 21:27:12 +0530

    - Add the blob service and blob controller

--- AzureBlobProject/Services/ContainerService.cs
+++ AzureBlobProject/Services/ContainerService.cs
@@ -15,7 +15,7 @@ namespace AzureBlobProject.Services
         public async Task CreateContainer(string containerName)
         {
             BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
-            await blobContainerClient.CreateIfNotExistsAsync();
+            await blobContainerClient.CreateIfNotExistsAsync(PublicAccessType.BlobContainer);
         }
 
         public async Task DeleteContainer(string containerName)
@@ -34,9 +34,33 @@ namespace AzureBlobProject.Services
             return containerNameList;
         }
 
-        public Task<List<string>> GetAllContainerAndBlobs()
+        public async Task<List<string>> GetAllContainerAndBlobs()
         {
-            throw new NotImplementedException();
+            List<string> containerAndBlobNames = new();
+            containerAndBlobNames.Add("Account Name : " + _blobServiceClient.AccountName);
+            containerAndBlobNames.Add("------------------------------------------------------------------------------------------------------------");
+            await foreach (BlobContainerItem blobContainerItem in _blobServiceClient.GetBlobContainersAsync())
+            {
+                containerAndBlobNames.Add("-->" + blobContainerItem.Name);
+                BlobContainerClient _blobContainer =
+                      _blobServiceClient.GetBlobContainerClient(blobContainerItem.Name);
+                await foreach (BlobItem blobItem in _blobContainer.GetBlobsAsync())
+				{ 
+                    //get metadata
+					var blobClient = _blobContainer.GetBlobClient(blobItem.Name);
+					BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
+					string blobToAdd = blobItem.Name;
+					if (blobProperties.Metadata.ContainsKey("title"))
+					{
+						blobToAdd += "(" + blobProperties.Metadata["title"] + ")";
+					}
+
+					containerAndBlobNames.Add("------" + blobToAdd);
+				}
+                containerAndBlobNames.Add("------------------------------------------------------------------------------------------------------------");
+
+            }
+            return containerAndBlobNames;
         }
     }
 }
